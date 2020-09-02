91e5545133e3f23b12fd087a34cc8abe70d55596
royFloresNyc <royfloresnyc@gmail.com>
2020-09-02 11:56:25 -0400

    Appearance sort method in Guest model

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,4 +2,7 @@ class Guest < ApplicationRecord
     has_many :appearances
     has_many :episodes, through: :appearances
 
+    def sorted_appearances
+        self.appearances.sort {|a, b| a.rating <=> b.rating }.reverse
+    end 
 end
