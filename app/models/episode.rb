bd17e07cd417a77e429a3585b86c61488d37a065
jhonny689 <jhonny.chamoun@gmail.com>
2020-09-02 11:07:44 -0400

    deliverable # 7

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,10 @@
 class Episode < ApplicationRecord
     has_many :appearances
     has_many :guests, through: :appearances
+
+    def avg_rating
+        self.appearances.reduce(0.0) do |total_rating, appearance|
+            total_rating + appearance.rating
+        end/self.appearances.count
+    end
 end
