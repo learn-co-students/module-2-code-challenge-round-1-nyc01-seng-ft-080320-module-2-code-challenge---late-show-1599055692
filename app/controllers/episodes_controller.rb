00caaf0583402c5c7dd6e60494113c80ca110dde
jhonny689 <jhonny.chamoun@gmail.com>
2020-09-02 10:43:35 -0400

    deliverable # 3

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,6 +1,16 @@
 class EpisodesController < ApplicationController
 
+  before_action :get_episode_by_id, only: [:show]
   def index
     @episodes = Episode.all
+  end
+
+  def show
+
+  end
+
+  private
+  def get_episode_by_id
+    @episode = Episode.find(params[:id])
   end
 end
