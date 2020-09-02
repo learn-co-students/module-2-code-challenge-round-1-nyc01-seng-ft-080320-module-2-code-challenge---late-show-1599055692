21eca9fb6386b1abdd8610ffb20b551c9a6ef795
anickel101 <anson.nickel@gmail.com>
2020-09-02 11:53:09 -0400

    Added Guest Episode Sorted By Appearance Rating

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,9 @@
 class Guest < ApplicationRecord
     has_many :appearances, dependent: :destroy
     has_many :episodes, through: :appearances
+
+    def sorted_appearances
+        self.appearances.sort_by{|appr| appr.rating}.reverse
+    end
+
 end
