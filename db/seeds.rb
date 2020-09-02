09b34e2686248c4c39ed9fcf603ee44974b7f8a5
AmSmo <adam.smolenski@gmail.com>
2020-09-02 11:14:53 -0400

    before ordering button

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Guest.destroy_all
+# Guest.destroy_all
 
-require 'csv'
+# require 'csv'
 
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'daily_show_guests.csv'))
-csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-csv.first(100).each do |row|
-  g = Guest.find_or_initialize_by(name: row['Raw_Guest_List'].split(',').first )
-  g.occupation = row['GoogleKnowlege_Occupation']
-  g.save
+# csv_text = File.read(Rails.root.join('lib', 'seeds', 'daily_show_guests.csv'))
+# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+# csv.first(100).each do |row|
+#   g = Guest.find_or_initialize_by(name: row['Raw_Guest_List'].split(',').first )
+#   g.occupation = row['GoogleKnowlege_Occupation']
+#   g.save
+# end
+
+# date = Date.parse('2015-09-08')
+
+# (1..40).each do |num|
+#   Episode.create(date: date, number: num)
+#   date = date.next
+# end
+
+
+60.times do 
+  Appearance.create(guest: Guest.all.sample, episode: Episode.all.sample, rating: rand(1..5) )
 end
-
-date = Date.parse('2015-09-08')
-
-(1..40).each do |num|
-  Episode.create(date: date, number: num)
-  date = date.next
-end
-
