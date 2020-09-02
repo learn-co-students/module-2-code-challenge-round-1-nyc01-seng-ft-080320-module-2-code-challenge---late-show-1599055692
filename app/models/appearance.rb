c2d6d637a7581b0fd0f272b942f6452d094b8ef4
anickel101 <anson.nickel@gmail.com>
2020-09-02 11:49:08 -0400

    Added One-Guest Validation

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -3,6 +3,12 @@ class Appearance < ApplicationRecord
     belongs_to :episode
 
     validates :rating, inclusion: 1..5
-    
+    validate :one_guest_only, on: :create
+
+    def one_guest_only
+        if episode.guests.include?(guest)
+            errors.add(:guest, "is already a guest on this episode.")
+        end
+    end
 
 end
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -5,7 +5,12 @@ class Episode < ApplicationRecord
     def average_rating
         total_rating = self.appearances.sum {|appr| appr.rating}
         total_appearances = self.appearances.count.to_f
-        total_rating/total_appearances
+        avg_rating = (total_rating/total_appearances).round(2)
     end
+
+    # Definitely not necessarily to match names...just match guests instead. See Apperance model.
+    # def guest_names
+    #     self.guests.map {|guest| guest.name}
+    # end
 
 end
