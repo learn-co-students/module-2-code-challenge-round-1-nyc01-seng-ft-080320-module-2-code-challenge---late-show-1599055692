638b68a8a360325ac5a3a9f3e85676187fa5929d
AJavier55 <aliciajavier5@gmail.com>
2020-09-02 11:58:38 -0400

    added one of the advanced deliverable

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -3,4 +3,5 @@ class Appearance < ApplicationRecord
     belongs_to :guest
 
     validates :rating, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+    validates :guest, uniqueness: { scope: :episode, message: "is already in this episode"}
 end
