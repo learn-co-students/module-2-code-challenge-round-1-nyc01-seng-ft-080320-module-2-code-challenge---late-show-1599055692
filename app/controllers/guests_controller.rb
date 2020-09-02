37a901e190cd835020ba51f477ccdba44d8517f4
jhonny689 <jhonny.chamoun@gmail.com>
2020-09-02 10:53:54 -0400

    deliverable # 4

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,6 +1,16 @@
 class GuestsController < ApplicationController
-
+  before_action :get_guest_by_id, only: [:show]
+  
   def index
     @guests = Guest.all
+  end
+
+  def show
+
+  end
+
+  private
+  def get_guest_by_id
+    @guest = Guest.find(params[:id])
   end
 end
