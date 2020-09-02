2d61472b35948072cc4030fd16cde26a2763ea9d
CanniJr <cannigialaluw@gmail.com>
2020-09-02 11:58:36 -0400

    Core deliverables done

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -1,2 +1,22 @@
 class AppearancesController < ApplicationController
+    def new
+        @appearance = Appearance.new
+    end
+
+    def create
+        @appearance = Appearance.create(appearance_params)  #to myself: use the variable on both if statements
+
+        if @appearance.valid?
+            redirect_to episode_path(@appearance.episode)
+        else
+            flash[:error_message] = @appearance.errors.full_messages
+            redirect_to new_appearance_path
+        end
+    end
+
+    private
+
+    def appearance_params
+        params.require(:appearance).permit(:rating, :guest_id, :episode_id)
+    end
 end
