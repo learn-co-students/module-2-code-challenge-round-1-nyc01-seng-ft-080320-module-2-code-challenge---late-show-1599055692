5640e12f36456e65913ef3bb52262bcceb49b690
slurio <samantha.lurio@gmail.com>
2020-09-02 11:48:12 -0400

    completed advanced #7

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -2,4 +2,9 @@ class Episode < ApplicationRecord
     has_many :appearances
     has_many :guests, through: :appearances
 
+    def average_rating
+        self.appearances.map do |appearance|
+            appearance.rating
+        end.sum/(self.appearances.length)
+    end
 end
