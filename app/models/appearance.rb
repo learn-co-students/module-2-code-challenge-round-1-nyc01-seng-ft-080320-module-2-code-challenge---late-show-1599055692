f20d9de761ac2d6d81d22cb4215f43ad2edbcff8
Mattie43 <ericksenm93@ymail.com>
2020-09-02 11:03:20 -0400

    core deliverables done, working on adv

---
+++ app/models/appearance.rb
@@ -0,0 +1,13 @@
+class Appearance < ApplicationRecord
+  belongs_to :episode
+  belongs_to :guest
+
+  validates :rating, inclusion: { in: (1..5), message: "must be between 1 and 5" }
+  validate :guest_appears_once
+
+  def guest_appears_once
+    if Appearance.find_by(guest: guest_id, episode: episode_id) != nil
+      errors.add(:guest, "can't be on the same episode twice")
+    end
+  end
+end
