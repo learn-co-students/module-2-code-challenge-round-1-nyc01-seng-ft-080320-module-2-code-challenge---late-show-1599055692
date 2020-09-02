2d217ceaa5a4868d7ee3e7967a6fff7ab30df249
AmSmo <adam.smolenski@gmail.com>
2020-09-02 11:24:36 -0400

    Sort button added...

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -2,6 +2,11 @@ class GuestsController < ApplicationController
   before_action :find_guest, only: [:show]
 
   def show
+    if params[:sort] == "rating"
+      @appearances = @guest.appearances.sort_by {|appearance| appearance.rating}.reverse
+    else
+      @appearances = @guest.appearances
+    end
   end
 
   def index
