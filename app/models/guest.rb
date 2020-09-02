d640ae4863f98487c59e14d149f3750b7b28ff9c
islommashanlo <islom.mashanlo@gmail.com>
2020-09-02 11:23:40 -0400

    core done

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,8 @@
 class Guest < ApplicationRecord
+    has_many :appearances
+    has_many :episodes, through: :appearances
 
+    def episodes_sorted
+        self.epsiodes.sort_by!{|e|e.rating}
+    end
 end
