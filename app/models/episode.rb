b5f7be463d93121f576b3d4a51508c7079d135eb
anickel101 <anson.nickel@gmail.com>
2020-09-02 11:07:51 -0400

    Advanced: Average Rating Added

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,11 @@
 class Episode < ApplicationRecord
     has_many :appearances
     has_many :guests, through: :appearances
+
+    def average_rating
+        total_rating = self.appearances.sum {|appr| appr.rating}
+        total_appearances = self.appearances.count.to_f
+        total_rating/total_appearances
+    end
+
 end
