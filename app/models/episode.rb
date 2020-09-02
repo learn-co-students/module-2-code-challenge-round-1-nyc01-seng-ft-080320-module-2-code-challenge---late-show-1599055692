0fe48165d7e425c279dd7eb561e9afe633bc6ed9
russellsutt <russell.sutter@gmail.com>
2020-09-02 12:00:18 -0400

    Super duper donzo.

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -2,8 +2,6 @@ class Episode < ApplicationRecord
     has_many :appearances
     has_many :guests, through: :appearances
 
-
-
     def average_rating
         total_rating= 0.0
         self.appearances.each do |appearance|
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,6 +1,4 @@
 class Guest < ApplicationRecord
     has_many :appearances
     has_many :episodes, through: :appearances
-
-    
 end
