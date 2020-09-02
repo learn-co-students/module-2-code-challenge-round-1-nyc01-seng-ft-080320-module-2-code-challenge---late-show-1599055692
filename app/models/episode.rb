9d2734e423f4df6de465ca5d005d8daf5db57866
royFloresNyc <royfloresnyc@gmail.com>
2020-09-02 11:51:54 -0400

    Episode Average Rating

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,8 @@
 class Episode < ApplicationRecord
     has_many :appearances
     has_many :guests, through: :appearances
+
+    def average_rating 
+        self.appearances.reduce(0) {|sum, appearance| sum + appearance.rating} / self.appearances.count
+    end 
 end
