d6d6421ca9e32c5015f32a8220f3cc9260c6150f
slurio <samantha.lurio@gmail.com>
2020-09-03 10:59:02 -0400

    finished last advanced #8

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,10 @@
 class Guest < ApplicationRecord
     has_many :appearances
     has_many :episodes, through: :appearances
+
+    def rating_sort
+        self.appearances.sort_by do |appearance|
+            appearance.rating
+        end
+    end
 end
